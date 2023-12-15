904af8e481c1abb50aa26886097fe3ddec51d9be
Ricardo Falcao <ricardolfalcao.2005@gmail.com>
2023-12-14 22:47:14 -0500

    Models Set les variables pour lest tables

---
+++ ResumeBuilder/Models/WorkExperience.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeBuilder.Models
+{
+    internal class WorkExperience
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Role { get; set; }
+
+        public string YearsWorked { get; set; }
+    }
+}
